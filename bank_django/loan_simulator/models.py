acca2346eb1ff50467ed2d308f9822167a302963
httpirsh <iris.frias.sousa@gmail.com>
2024-12-17 19:05:49 +0000

    Experiments with django

---
+++ bank_django/loan_simulator/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class LoanSimulation(models.Model):
+    amount = models.IntegerField("Amount (€)")
+    duration = models.IntegerField("Duration (months)", null=True, blank=True)
+    desired_monthly_payment = models.IntegerField("Desired monthly payment (€)", null=True, blank=True)
+
+    def clean(self):
+        super().clean()
+        if self.duration and self.desired_monthly_payment:
+            raise ValidationError("You can only set 'duration' or 'desired_monthly_payment', not both.")
+        if not self.duration and not self.desired_monthly_payment:
+            raise ValidationError("You must set either 'duration' or 'desired_monthly_payment'.")
+
+class LoanDetails(models.Model):
+    #loan_simulation = models.ForeignKey(LoanSimulation)
+    interest_rate = models.FloatField("Interest rate (%)")
+    total_repayment = models.FloatField("Total repayment")
+    monthly_payment = models.IntegerField("Monthly payment (€)")
+
+class LoanApplication(models.Model):
+    monthly_income = models.IntegerField("Monthly income (€)")
+    monthly_expenses = models.IntegerField("Monthly expenses (€)")
---
+++ bank_django/loan_simulator/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from loan_simulator.models import LoanSimulation
+
+class LoanTestCase(TestCase):
+
+    def test_desired_monthly_payment(self):
+        """
+        Loan with 'amount' and 'desired_monthly_payment'.
+        """
+        monthly_loan = LoanSimulation.objects.create(amount=10000, desired_monthly_payment=10)
+        monthly_loan.full_clean()
+
+    def test_duration(self):
+        """
+        Loan with 'amount' and 'duration'.
+        """
+        duration_loan = LoanSimulation.objects.create(amount=10000, duration=36)
+        duration_loan.full_clean()
+
+    def test_cannot_set_monthly_and_duration(self):
+        """
+        Loan can only set 'duration' or 'desired_monthly_payment', not both.
+        """
+        loan = LoanSimulation.objects.create(amount=10000, duration=36, desired_monthly_payment=10)
+        with self.assertRaises(ValidationError) :
+            loan.full_clean()
+
+    def test_must_set_monthly_or_duration(self):
+        """
+        Loan cannot have 'duration' and 'desired_monthly_payment' empty.
+        """
+        loan = LoanSimulation.objects.create(amount=1000)
+        with self.assertRaises(ValidationError):
+            loan.full_clean()
