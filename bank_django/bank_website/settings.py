8df6a6625f56e8c70de5fa8b0ba53de8fc41c092
Íris Sousa <iris.frias.sousa@gmail.com>
2024-12-30 19:15:10 +0000

    Fix local access

--- bank_django/bank_website/settings.py
+++ bank_django/bank_website/settings.py
@@ -128,8 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
+#STATIC_ROOT = "frontend/dist/assets"
+#STATIC_URL = "/assets/"
+STATIC_ROOT = "static"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
